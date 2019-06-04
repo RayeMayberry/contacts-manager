9d5e46bfa6c517f9da3f1e36f12f35ac546c5fb0
Raye <rayemayberry@yahoo.com>
2019-06-04 18:24:04 -0500

    yay routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CompanyInfoComponent} from './company-info/company-info.component'
+import {CompanyLookupComponent} from './company-lookup/company-lookup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CompanyInfoComponent } ,
+  { path: 'lookup', component: CompanyLookupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
