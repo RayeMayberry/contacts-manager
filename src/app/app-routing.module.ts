b220f5906f4eef441694e3e74c69f653cb6bb94f
Raye <rayemayberry@yahoo.com>
2019-06-08 03:06:34 -0500

    add table to contacts module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CompanyInfoComponent} from './company-info/company-info.component'
 import {CompanyLookupComponent} from './company-lookup/company-lookup.component';
+import{CreateCategoryComponent} from './create-category/create-category.component';
+import{PhoneLookupComponent} from './phone-lookup/phone-lookup.component';
 
 const routes: Routes = [
   { path: '', component: CompanyInfoComponent } ,
-  { path: 'lookup', component: CompanyLookupComponent }
+  { path: 'lookup', component: CompanyLookupComponent },
+  { path: 'create-category', component: CreateCategoryComponent},
+  { path: 'phone-lookup', component: PhoneLookupComponent}
 ];
 
 @NgModule({
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -13,3 +13,4 @@ export class ContactsComponent implements OnInit {
   }
 
 }
+
